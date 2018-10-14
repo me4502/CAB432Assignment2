3c2647b618f5162b562c9a35f31da703c292cf3f
Matthew Miller <mnmiller1@me.com>
2018-10-14 16:33:23 +1000

    Make HTML & JS Compliant

--- client/js/project.js
+++ client/js/project.js
@@ -1,15 +1,16 @@
 /**
- * This function checks if a user ID input is empty. 
- * 
+ * This function checks if a user ID input is empty.
+ *
  * return true if the user ID is not empty.
  * return false if the user ID is empty.
+ * @return {boolean}
  */
-function IsUserIDEmpty(){
+function isUserIdEmpty(){
   var isValid = true;
   var errorMessage = "Please enter a user ID";
   var userID = document.getElementById("icon_prefix").value;
 
-  if (userID == ""){
+  if (userID === ""){
     alert(errorMessage);
     isValid = false;
   }
